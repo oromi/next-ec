9ae5b452a1178942ddb74270e43656fa50563f6b
oromi <exemplumluna+git@gmail.com>
2022-08-18 11:32:42 +0900

    add stripe api

---
+++ pages/api/products.js
@@ -0,0 +1,35 @@
+import Stripe from 'stripe'
+
+export default async function handler(req, res) {
+  if(req.method.toLocaleLowerCase() !== 'get') {
+    return res.status(405).end()
+  }
+  const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+    apiVersion: '2022-08-01',
+    maxNetworkRetries: 3
+  })
+  const products = await stripe.products.list()
+  if (!products.data || products.data.length < 1){
+    return res.status(200).json([])
+  }
+  const response = await Promise.all(products.data.map(async (product, i) => {
+    const prices = await stripe.prices.list({
+        product: product.id,
+    })
+    return {
+        id: product.id,
+        description: product.description,
+        name: product.name,
+        images: product.images,
+        unit_label: product.unit_label,
+        prices: prices.data.map(price => {
+            return {
+                id: price.id,
+                currency: price.currency,
+                transform_quantity: price.transform_quantity,
+                unit_amount: price.unit_amount,
+            }
+        })
+    }
+  }))
+}
